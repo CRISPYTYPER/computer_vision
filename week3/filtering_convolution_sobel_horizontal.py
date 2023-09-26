48a5d960f090db91ec2885fcf84a51584e1bc199
박진수 <2019040591@hanyang.ac.kr>
2023-09-26 16:50:35 +0900

    implement edge detection filter

--- week3/filtering_convolution_sobel_horizontal.py
+++ week3/filtering_convolution_sobel_horizontal.py
@@ -13,25 +13,27 @@ print(img.shape)
 denoised_img = np.zeros((height, width, channel))
 
 ix_filter = np.zeros((3,3))
-ix_filter[1,1] = 1
-ix_filter[1,2] = -1
+ix_filter[:,0] = [1,2,1]
+ix_filter[:,2] = [-1,-2,-1]
 ix_filter= np.flip(ix_filter,axis=None)
 
 
+
 iy_filter = np.zeros((3,3))
-iy_filter[1,1] = 1
-iy_filter[2,1] = -1
+iy_filter[0,:] = [1,2,1]
+iy_filter[2,:] = [-1,-2,-1]
 iy_filter= np.flip(iy_filter, axis=None)
 
 for c in range(channel):
     for x in range(1, width-1):
         for y in range(1, height-1):
             tmp = img[y - 1:y + 2, x - 1:x + 2, c] # 3x3box
-            local_mean_ix = np.sum(ix_filter*tmp)
-            local_mean_iy = np.sum(iy_filter*tmp)
-            denoised_img[y][x][c] = (local_mean_ix**2 + local_mean_iy**2)**(1/2)
+            local_mean_ix = np.sum(ix_filter * tmp)
+            denoised_img[y][x][c] = local_mean_ix
 
 
+# Normalize the filtered image to the 0-255 range
+denoised_img = (denoised_img - np.min(denoised_img)) / (np.max(denoised_img) - np.min(denoised_img)) * 255
 
 # FLOAT 2 UINT8
 denoised_img = denoised_img.astype(np.uint8)
